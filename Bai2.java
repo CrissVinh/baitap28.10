4e5785ea18da870fea291e74f5b168ed9f36d80a
CrissVinh <nguyenphanvinh1601@gmail.com>
2024-10-28 14:40:47 +0700

    Add files via upload

---
+++ Bai2.java
@@ -0,0 +1,13 @@
+
+package bai2;
+
+public class Bai2 {
+
+    public static void main(String[] args) {
+        System.out.println("Ten: Nguyen Phan Vinh");
+        System.out.println("Mssv: 29219027455");
+	System.out.println("Class:CMU-CS 303 ");        
+        
+    }
+    
+}
---
+++ Bai2NC.java
@@ -0,0 +1,23 @@
+package bai2nc;
+
+import java.util.Scanner;
+
+public class Bai2NC {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String ten;
+        System.out.println(" Nhap ho va ten: ");
+        ten = sc.nextLine();
+        String mssv;
+        System.out.println(" Nhap ma so sinh vien: ");
+        mssv = sc.nextLine();
+        String lop;
+        System.out.println(" Nhap ten lop: ");
+        lop = sc.nextLine();
+        System.out.println("Ten: " + ten);
+        System.out.println("mssv: " + mssv);
+        System.out.println("lop: " + lop);        
+    }
+    
+}
